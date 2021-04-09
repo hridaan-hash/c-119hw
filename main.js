91351b0cfd3b9ca256df11d15c7d321118097fe7
hridaan-hash <73286294+hridaan-hash@users.noreply.github.com>
2021-04-09 15:46:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+function draw()
+{
+   image(video, 0, 0,300 ,300); 
+   classifier.classify(video, gotResult);
+}
+
+function gotResult()
+{
+    if (error) {
+console.error(error);
+} else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].confidence.toFixed(3)
+     }
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('');
+}
+    function draw()
+    {
+       image(video, 0, 0,300 ,300); 
+       classifier.classify(video, gotResult);
+    }
+    
+    function gotResult()
+    {
+        if (error) {
+    console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].confidence.toFixed(3)
+         }
+    }
+    
+    function setup()
+    {
+        canvas = createCanvas(300, 300);
+        canvas.center();
+        video = createCapture(VIDEO);
+        video.hide();
+        classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/igA6Rd2rS/model.json',modelLoaded) ;
+    }
+       function modelLoaded()
+       {
+           console.log('Model Loaded!');
+       } 
+
+   function modelLoaded()
+   {
+       console.log('Model Loaded!');
+   } 
